65f4230ddb2b517861cbeb9928e7c6f0f990b915
Marco Hao <hsm123@bupt.edu.cn>
2018-08-03 01:20:57 +0800

    index

---
+++ leetcode/数位dp_lc338.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+
+	bool is_parl[1001][1001];
+	int dp[1001][1001];
+
+	bool get_parl( string s,  )
+
+    int countSubstrings(string s) {
+        
+    }
+};
+
+int main()
+{
+	Solution s;
+	cout << s.countSubstrings("abc");
+	return 0;
+}
